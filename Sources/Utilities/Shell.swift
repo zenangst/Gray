a5e3c962c8ccb780c7c551300c44e60646140cda
Christoffer Winterkvist <christoffer@winterkvist.com>
2018-10-18 20:45:16 +0200

    Refactor Shell implementation

--- Sources/Utilities/Shell.swift
+++ Sources/Utilities/Shell.swift
@@ -7,23 +7,55 @@ class Shell {
     let process = Process()
     let path = path.replacingOccurrences(of: " ", with: "\\ ")
     let arguments = arguments.joined(separator: " ")
-    let command = "cd \(path) && \(command) \(arguments) &"
+    let command = "cd \(path) && \(command) \(arguments)"
     return process.shell(command: command)
   }
 }
 
-
 extension Process {
   public func shell(command: String) -> String {
-    let pipe = Pipe()
+    let outputQueue = DispatchQueue.init(label: "shell-queue")
+    let outputPipe = Pipe()
+    let errorPipe = Pipe()
 
     launchPath = "/bin/bash"
     arguments = ["-c", command]
-    standardOutput = pipe
+    standardOutput = outputPipe
+    standardError = errorPipe
+
+    var result = Data()
+    var error = Data()
+
+    outputPipe.fileHandleForReading.readabilityHandler = { handler in
+      outputQueue.async { result.append(handler.availableData) }
+    }
+
+    errorPipe.fileHandleForReading.readabilityHandler = { handler in
+      outputQueue.async { error.append(handler.availableData) }
+    }
 
     launch()
     waitUntilExit()
-    let data = pipe.fileHandleForReading.readDataToEndOfFile()
-    return String(data: data, encoding: String.Encoding.utf8) ?? ""
+
+    outputPipe.fileHandleForReading.readabilityHandler = nil
+    errorPipe.fileHandleForReading.readabilityHandler = nil
+
+    return outputQueue.sync {
+      return result.string()
+    }
+  }
+}
+
+fileprivate extension Data {
+  func string() -> String {
+    guard let output = String(data: self, encoding: .utf8) else { return "" }
+
+    guard !output.hasSuffix("\n") else {
+      let endIndex = output.index(before: output.endIndex)
+      return String(output[..<endIndex])
+    }
+
+    return output
+
   }
 }
