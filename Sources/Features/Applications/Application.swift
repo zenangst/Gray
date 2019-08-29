7ff04419a92fccf74cfdd643328c164571a530e5
Christoffer Winterkvist <christoffer@winterkvist.com>
2019-08-29 21:58:01 +0200

    Release 0.13.0

--- Sources/Features/Applications/Application.swift
+++ Sources/Features/Applications/Application.swift
@@ -14,17 +14,4 @@ struct Application: Hashable {
   let preferencesUrl: URL
   let appearance: Appearance
   let restricted: Bool
-
-  init(bundleIdentifier: String, name: String,
-       metadata: String,
-       url: URL, preferencesUrl: URL,
-       appearance: Appearance, restricted: Bool) {
-    self.bundleIdentifier = bundleIdentifier
-    self.name = name
-    self.metadata = metadata
-    self.url = url
-    self.preferencesUrl = preferencesUrl
-    self.appearance = appearance
-    self.restricted = restricted
-  }
 }
